4ba40604033c0ebdd7276c167670010ed3e3b528
Chris <c.bailey00@hotmail.com>
2023-01-04 11:28:39 +0000

    finished test driving form to add new note

--- notesView.js
+++ notesView.js
@@ -2,14 +2,33 @@ class NotesView{
   constructor(model){
     this.model = model;
     this.mainContainerEl = document.querySelector('#main-container');
+
+    this.input = document.querySelector('#note-input')
+    this.button = document.querySelector('#add-note-btn');
+
+    this.button.addEventListener('click', () => {
+      this.model.addNote(this.input.value);
+      this.updateNotes();
+    });
   }
   displayNotes() {
+    this.mainContainerEl.innerHTML = '';
+
     this.model.getNotes().forEach(note => {
       const newNote = document.createElement('div');
       newNote.className = 'note';
       newNote.textContent = note;
       this.mainContainerEl.append(newNote);
   });
+  }
+
+  updateNotes() {
+    const note = this.model.getNotes().at(-1);
+    console.log(note)
+    const newNote = document.createElement('div');
+    newNote.className = 'note';
+    newNote.textContent = note;
+    this.mainContainerEl.append(newNote);
   }
 }
 
--- notesView.test.js
+++ notesView.test.js
@@ -41,5 +41,18 @@ describe('NotesView class', () => {
     view.displayNotes();
 
     expect(document.querySelectorAll('div.note').length).toBe(2);
+  });
+
+  it('correctly displays an inputted note using a click', () => {
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    const input = document.querySelector('#note-input');
+    const button = document.querySelector('#add-note-btn');
+    
+    input.value = 'hello notes';
+    button.click();
+
+    expect(document.querySelector('div.note').textContent).toEqual('hello notes');
   })
 })
